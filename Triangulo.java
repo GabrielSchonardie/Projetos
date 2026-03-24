337bf7a6676409257367ed64cbc354fa21315c72
GabrielSchonardie <gschonardiedasilva@gmail.com>
2026-03-24 15:33:32 -0300

    Cálculo de hipotenusa, perímetro e área.java

--- Triangulo.java
+++ Triangulo.java
@@ -2,22 +2,25 @@ import java.util.Scanner;
 
 public class Triangulo {
 
-    public static void main (String [] args ) {
+    public static void main(String[] args) {
 
         Scanner read = new Scanner(System.in);
 
-        System.out.print("Digite um cateto(em cm): ");
-        int catetoUm = read.nextInt();
+        System.out.print("Digite um cateto (cm): ");
+        double catetoUm = read.nextDouble();
 
-        System.out.print("Digite outro cateto(em cm): ");
-        int catetoDois = read.nextInt();
+        System.out.print("Digite outro cateto (cm): ");
+        double catetoDois = read.nextDouble();
 
-        double valorH = (Math.pow(catetoUm, 2)) + (Math.pow(catetoDois, 2));
-        int hipotenusa = (int)Math.sqrt(valorH);
-        System.out.println("\nHipotenusa: " + hipotenusa +" cm");
-        System.out.println("Perímetro: " + (catetoDois + catetoUm + hipotenusa) + " cm");
-        double area = (catetoUm * catetoDois) / 2;
-        System.out.print("Área: " + area + " cm²");
+        double somaQuadrados = Math.pow(catetoUm, 2) + Math.pow(catetoDois, 2);
+        double hipotenusa = Math.sqrt(somaQuadrados);
+
+        double perimetro = catetoUm + catetoDois + hipotenusa;
+        double area = (catetoUm * catetoDois) / 2.0;
+
+        System.out.printf("\nHipotenusa: %.2f cm", hipotenusa);
+        System.out.printf("\nPerímetro: %.2f cm", perimetro);
+        System.out.printf("\nÁrea: %.2f cm²", area);
 
         read.close();
     }
