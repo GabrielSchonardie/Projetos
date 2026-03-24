0768c5a6442b42e9f14a09c426ce297a8691aa7a
GabrielSchonardie <gschonardiedasilva@gmail.com>
2026-03-24 13:57:37 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class Main {
+
+    public static void main (String[] args) {
+
+        Scanner read = new Scanner(System.in);
+
+        System.out.print("\nDigite o nome do produto: ");
+        String nomeProduto = read.nextLine();
+
+        System.out.print("\nDigite a quantidade do produto: ");
+        int quantidade = read.nextInt();
+
+        System.out.print("\nDigite o preço do produto: R$ ");
+        double preco = read.nextDouble();
+
+        System.out.printf(
+                "\nProduto: %s - Quantidade: %d - Preço: R$ %.2f ",
+                nomeProduto, quantidade, preco
+        );
+
+        read.close();
+    }
+}
